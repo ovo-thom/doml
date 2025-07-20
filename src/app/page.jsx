63dfdd3b2fe77c517f4f6f3c64a6fc5a81d89f17
Thomas OvO <thonnard.thomas@gmail.com>
2025-07-21 00:59:22 +0200

    feat: add Navbar.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <div className="min-h-screen">
-     Doml
+    <div className="min-h-screen bg-black w-full">
+      <div className="max-w-5xl h-screen border-x border-x-gray-400 mx-auto">
+<Navbar />
+      </div>
     </div>
   );
 }
