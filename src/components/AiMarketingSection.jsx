ca17bf511fc1373245836d0865dcaf645c9579a4
Thomas OvO <thonnard.thomas@gmail.com>
2025-07-22 23:48:44 +0200

    feat: add burger menu responsive Navbar

--- src/components/AiMarketingSection.jsx
+++ src/components/AiMarketingSection.jsx
@@ -6,7 +6,7 @@ import arrowright from "../assets/icons/arrowright.png";
 
 export default function AiMarketingSection() {
   return (
-    <section className="text-white h-screen my-32">
+    <section className="text-white h-screen md:my-32">
       <div className="grid grid-cols-1 sm:grid sm:grid-cols-[60%_40%] justify-center items-center">
         <div className="w-full order-2 sm:order-1">
           <Image
@@ -14,6 +14,7 @@ export default function AiMarketingSection() {
             height="auto"
             src={aimarketing}
             alt="aimarketing"
+            className="w-64 sm:w-[400px] mx-auto sm:ml-0 sm:mr-0 my-4"
           />
 
           <Image
@@ -21,14 +22,15 @@ export default function AiMarketingSection() {
             height="auto"
             src={optimizedreach}
             alt="optimized reach"
+            className="w-80 sm:w-[500px] mx-auto sm:ml-0 sm:mr-0"
           />
-          <p className=" text-lg w-[77%] mt-8">
+          <p className=" text-lg w-[77%] mt-8 mx-auto sm:ml-0 sm:mr-0">
             Our vision is to revolutionize the way brands and advertisers
             target, reach
           </p>
-          <span className="block bg-gradient-to-r from-[#FF2DF7] via-[#5200FF] to-[#00F0FF] h-[1px] my-10 w-3/4"></span>
+          <span className="block bg-gradient-to-r from-[#FF2DF7] via-[#5200FF] to-[#00F0FF] h-[1px] my-10 w-3/4 mx-auto md:ml-0 md:mr-0"></span>
           <div className="bg-gradient-to-r from-[#FF2DF7] via-[#5200FF] to-[#00F0FF] p-[1px] rounded-2xl w-36">
-            <div className="flex justify-center items-center bg-black rounded-2xl py-1">
+            <div className="flex justify-center items-center bg-black rounded-2xl py-1 w-full">
               <button className="pr-2">Get started</button>
               <Image width={14} height={4} src={arrowright} alt="arrow right" />
             </div>
@@ -36,7 +38,7 @@ export default function AiMarketingSection() {
         </div>
 
         <div className="flex justify-center order-1 sm:order-2">
-          <Image height={450} width={450} src={planet} alt="planet" className="max-w-[200px] w-full h-auto" />
+          <Image height={450} width={450} src={planet} alt="planet" className="max-w-[200px] sm:max-w-none w-full h-auto" />
         </div>
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,30 @@ import logoDoml from "../assets/icons/logoDoml.png";
 import doml from "../assets/icons/doml.png";
 import arrowBottom from "../assets/icons/arrowbottom.png";
 import Image from "next/image";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Navbar() {
   return (
-    <nav className="text-white border-b h-20 flex items-center">
-      <div className="flex justify-between items-center w-full">
+    <>
+      {/* Mobile navbar */}
+      <nav className="flex justify-between bg-gray-950 p-5 md:hidden">
         <div className="flex items-center">
-          <div className="flex items-center">
-            <Image src={logoDoml} alt="logo dml" />
-            <Image src={doml} alt="title doml" />
-          </div>
+          <Image src={logoDoml} alt="logo dml" />
+          <Image src={doml} alt="title doml" />
+        </div>
+        <div>
+          <RxHamburgerMenu className="text-white text-2xl" />
+        </div>
+      </nav>
+
+      {/* Desktop navbar */}
+      <nav className="text-white border-b h-20 items-center justify-between bg-gray-950 hidden md:flex px-5">
+        <div className="flex items-center">
+          <Image src={logoDoml} alt="logo dml" />
+          <Image src={doml} alt="title doml" />
           <ul className="flex text-sm gap-x-3 ml-4 items-center">
             <li className="flex items-center">
-              <a href="#">Product</a>{" "}
+              <a href="#">Product</a>
               <Image src={arrowBottom} alt="arrow bottom" />
             </li>
             <li>
@@ -24,32 +35,30 @@ export default function Navbar() {
               <a href="#">Enterprise</a>
             </li>
             <li className="flex items-center">
-              <a href="#">Explore</a>{" "}
-              <Image src={arrowBottom} alt="arrow bottom" />{" "}
+              <a href="#">Explore</a>
+              <Image src={arrowBottom} alt="arrow bottom" />
             </li>
             <li>
               <a href="#">MarketPlace</a>
             </li>
             <li className="flex items-center">
-              <a href="#">Pricing</a>{" "}
-              <Image src={arrowBottom} alt="arrow bottom" />{" "}
+              <a href="#">Pricing</a>
+              <Image src={arrowBottom} alt="arrow bottom" />
             </li>
           </ul>
         </div>
-        <div>
-          <div className="flex space-x-3 mr-3">
-            <input
-              type="search"
-              placeholder="Search Doml"
-              className="border border-[#A3A3A3] py-1 px-2 placeholder:text-[#C0C3C9] rounded outline-none bg-[#272F43] text-gray-300"
-            />
-            <button className="px-2">Sign in</button>
-            <button className="border rounded-lg px-2  border-[#9E9C9C]">
-              Sign up
-            </button>
-          </div>
+        <div className="flex space-x-3 mr-3">
+          <input
+            type="search"
+            placeholder="Search Doml"
+            className="border border-[#A3A3A3] py-1 px-2 placeholder:text-[#C0C3C9] rounded outline-none bg-[#272F43] text-gray-300"
+          />
+          <button className="px-2">Sign in</button>
+          <button className="border rounded-lg px-2 border-[#9E9C9C]">
+            Sign up
+          </button>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
